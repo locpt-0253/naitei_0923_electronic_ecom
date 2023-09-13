e5f777afda3c8b98e6696cb43747cd940ec35e16
tuanla-0386 <le.anh.tuan-c@sun-asterisk.com>
2023-09-13 10:57:49 +0700

    init_login_logout

---
+++ tests/Feature/Auth/PasswordConfirmationTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Tests\Feature\Auth;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class PasswordConfirmationTest extends TestCase
+{
+    use RefreshDatabase;
+}
---
+++ tests/Feature/Auth/RegistrationTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Tests\Feature\Auth;
+
+use App\Providers\RouteServiceProvider;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class RegistrationTest extends TestCase
+{
+    use RefreshDatabase;
+}
